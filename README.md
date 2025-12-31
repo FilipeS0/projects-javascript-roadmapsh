28d4d9009b1811612ce800691c08344490b5fafe
Filipe Souto <filipesoutocosta@gmail.com>
2025-12-31 11:19:50 -0300

    job done

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # projects-javascript-roadmapsh
+
 I'm concentrating my roadmap.sh projects in this repository.
 
 ## Tabs
+
 [See the goal](https://roadmap.sh/projects/simple-tabs) which I had to go to.  
 [See my repository](https://github.com/FilipeS0/projects-javascript-roadmapsh/tree/main/tabs) that's what I did.
+
+## cookie-consent
+
+[See the goal](https://roadmap.sh/projects/cookie-consent) which I had to go to.  
+[See my repository](https://github.com/FilipeS0/projects-javascript-roadmapsh/tree/main/cookie-consent) that's what I did.
--- cookie-consent/assets/js/main.js
+++ cookie-consent/assets/js/main.js
@@ -1,2 +1,18 @@
 const bClose = document.getElementById("close");
-const save = document.getElementById("accept");
+const accept = document.getElementById("accept");
+const cookie = document.querySelector(".cookie");
+
+if (localStorage.getItem("cookieConsent") === "accepted") {
+    closeCookieWindow();
+}
+
+function closeCookieWindow() {
+    cookie.classList.add("close");
+}
+
+bClose.addEventListener("click", closeCookieWindow);
+
+accept.addEventListener("click", () => {
+    localStorage.setItem("cookieConsent", "accepted");
+    closeCookieWindow();
+});
